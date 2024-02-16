f53e04ac861425477b2172db7f1555920356ab9a
JangSeongYeong <syzxz0608@gmail.com>
2024-02-16 09:18:13 +0900

    employees 테이블 데이터

---
+++ DB/data.sql
@@ -0,0 +1,218 @@
+select * from employees;
+desc employees;
+
+-- 데이터 시트 데이터 넣기
+insert into employees(
+	emp_no,
+	first_name,
+	family_name,
+	email,
+	mobile,
+	salary,
+	depart_no
+)values(
+	117,
+	'상민',
+	'전',
+	'susemi@naver.com',
+	'01012341234',
+	4500000,
+	'dev003'
+);
+
+insert into employees(
+	emp_no,
+	first_name,
+	family_name,
+	email,
+	mobile,
+	salary,
+	depart_no
+)values(
+	118,
+	'준',
+	'유',
+	'seller@email.co.kr',
+	'01012341234',
+	7400000,
+	'dev003'
+);
+
+insert into employees(
+	emp_no,
+	first_name,
+	family_name,
+	email,
+	mobile,
+	salary,
+	depart_no,
+	commision
+)values(
+	119,
+	'항오',
+	'조',
+	'search@daum.net',
+	'01012341234',
+	4500000,
+	'dev003',
+	90
+);
+
+insert into employees(
+	emp_no,
+	first_name,
+	family_name,
+	email,
+	mobile,
+	salary,
+	depart_no,
+	commision
+)values(
+	120,
+	'지훈',
+	'김',
+	'sunset@naver.com',
+	'01012341234',
+	2000000,
+	'dev003',
+	90
+);
+
+insert into employees(
+	emp_no,
+	first_name,
+	family_name,
+	email,
+	mobile,
+	salary,
+	depart_no,
+	commision
+)values(
+	121,
+	'형진',
+	'박',
+	'see@daum.net',
+	'01012341234',
+	5600000,
+	'dev004',
+	90
+);
+
+insert into employees(
+	emp_no,
+	first_name,
+	family_name,
+	email,
+	mobile,
+	salary,
+	depart_no,
+	commision
+)values(
+	122,
+	'덕기',
+	'이',
+	'zero10clzls@email.co.kr',
+	'01012341234',
+	8900000,
+	'dev004',
+	90
+);
+
+insert into employees(
+	emp_no,
+	first_name,
+	family_name,
+	email,
+	mobile,
+	salary,
+	depart_no,
+	commision
+)values(
+	123,
+	'혁준',
+	'권',
+	'sky11clzls@naver.com',
+	'01012341234',
+	6300000,
+	'dev004',
+	90
+);
+
+insert into employees(
+	emp_no,
+	first_name,
+	family_name,
+	email,
+	mobile,
+	salary,
+	depart_no,
+	commision
+)values(
+	124,
+	'기빈',
+	'고',
+	'zero12clzls@daum.net',
+	'01012341234',
+	4400000,
+	'dev004',
+	90
+);
+
+insert into employees(
+	emp_no,
+	first_name,
+	family_name,
+	email,
+	mobile,
+	salary,
+	depart_no,
+	commision
+)values(
+	125,
+	'강인',
+	'최',
+	'sky13clzls@email.co.kr',
+	'01012341234',
+	3700000,
+	'dev005',
+	90
+);
+
+-- 126부터 그냥
+insert into employees(
+	emp_no,
+	first_name,
+	family_name,
+	email,
+	mobile,
+	salary,
+	depart_no,
+	commision
+)values(
+	131,
+	'성환',
+	'유',
+	'sky03clzls@naver.com',
+	'01012341234',
+	4500000,
+	'dev002',
+	90
+);
+
+insert into employees(
+	emp_no,
+	first_name,
+	family_name,
+	email,
+	mobile,
+	salary,
+	depart_no
+)values(
+	132,
+	'영준',
+	'박',
+	'sky04clzls@email.co.kr',
+	'01012341234',
+	1200000,
+	'dev002'
+);

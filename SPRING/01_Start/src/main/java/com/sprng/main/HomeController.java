ae5af4497f40ec1069993a5f3c530a13b15a6ff5
JangSeongYeong <syzxz0608@gmail.com>
2024-03-14 19:30:22 +0900

    Spring Start

---
+++ SPRING/01_Start/src/main/java/com/sprng/main/HomeController.java
@@ -0,0 +1,27 @@
+package com.sprng.main;
+
+import java.util.Date;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);	
+
+	// get 메서드로 / 요청이 온다면 home 메서드를 실행 하라
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Model model) {		
+		model.addAttribute("msg","Hello SPRING"); // jsp 로 보낼 값을 저장
+		return "index"; // index.jsp 로 이동 해라
+	}
+	
+}
